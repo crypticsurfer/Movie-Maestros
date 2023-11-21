9dc6cebc88fd003e5168002009cbe6819d0b2a73
Cardon Hickman <cardonhickman@gmail.com>
2023-11-20 20:11:46 -0700

    added basic react stuff

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from './App.jsx'
-import './index.css'
+import Home from './pages/home';
+// import Signup from './pages/Signup';
+// // import Login from './pages/Login';
+// import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, 
+      // {
+      //   path: '/login',
+      //   element: <Login />
+      // }, 
+      // {
+      //   path: '/signup',
+      //   element: <Signup />
+      // }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
